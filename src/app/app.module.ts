0fcb3591a82f520e9abee33bc081c4bdddaf2880
Andres Felipe Osorio Silva <aosorios@pslcol.com.co>
2018-06-13 16:39:17 -0500

    Add heroes component class, html, css with mock-data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { OrdinalPipe } from './extras/ordinal.pipe';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    OrdinalPipe
   ],
   bootstrap: [ AppComponent ]
 })
---
+++ src/app/hero.ts
@@ -0,0 +1,6 @@
+export class Hero {
+  _name: string;
+  _height: number;
+  _picture: string;
+  _nickname: string
+}
