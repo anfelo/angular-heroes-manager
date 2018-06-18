9fd5b72ef785a490a028155d22db048079d0df5c
Andres Felipe Osorio Silva <aosorios@pslcol.com.co>
2018-06-18 10:48:24 -0500

    Add HTTP integration test to the HeroService

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,6 +1,42 @@
+import { TestBed } from '@angular/core/testing';
 import { HeroService } from './hero.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
 describe('HeroService', () => {
-  let heroService: HeroService;
+  let httpTestingController: HttpTestingController;
+  let service: HeroService;
 
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
+
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(HeroService);
+  });
+
+  describe('get', () => {
+
+    it('should call get with the correct URL', () => {
+
+      service.getHeroes().subscribe();
+
+      const req = httpTestingController.expectOne('https://udem.herokuapp.com/heroes');
+      req.flush([
+        {
+          _name: "Anthony Stark",
+          _height: 6.1,
+          _picture: "http://i.annihil.us/u/prod/marvel/i/mg/6/a0/55b6a25e654e6/standard_xlarge.jpg",
+          _nickname: "Iron Man"
+        },
+        {
+          _name: "Peter Parker",
+          _height: 5.1,
+          _picture: "http://i.annihil.us/u/prod/marvel/i/mg/9/30/538cd33e15ab7/standard_xlarge.jpg",
+          _nickname: "Spider-Man"
+        },
+      ]);
+      httpTestingController.verify();
+    });
+  });
 });
--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -12,27 +12,4 @@ describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let fixture: ComponentFixture<HeroesComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ 
-        HeroesComponent,
-        OrdinalPipe
-      ],
-      imports: [ 
-        RouterTestingModule,
-        StoreModule.forRoot({heroes: heroReducer}),
-      ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeroesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
